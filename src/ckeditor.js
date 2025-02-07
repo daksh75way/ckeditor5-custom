d05e9f14c6449b0a455c5d4e6be3b14886ea30fb
Lokesh <lokesh@75way.com>
2025-02-07 12:55:52 +0530

    paste from office plugin added

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,7 +1,3 @@
-/**
- * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
